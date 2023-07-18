d6fc60b5f6bb52461e1a7a8b4b498581ac4511ba
xGary <garyhe.05@gmail.com>
2023-07-18 17:54:38 +0800

    fix: remove x-frame-option

---
+++ pages/external/[id].js
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router';
+import WebsiteDetailsPage from 'components/pages/websites/WebsiteDetailsPage';
+
+export default function () {
+  const router = useRouter();
+  const { id } = router.query;
+
+  if (!id) {
+    return null;
+  }
+
+  return <WebsiteDetailsPage websiteId={id} />;
+}
